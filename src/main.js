1251fcd29ec4bf128a6b6b2787708d4de5814ec7
AvBorikDeveloper <avboris28@gmail.com>
2019-04-20 20:59:09 +1200

    map

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-let mySet = new Set(['Harry','Ron','hermione','Hagrid','Harry']);
+// let mySet = new Set(['Harry','Ron','hermione','Hagrid','Harry']);
 
 // for(let item of mySet){
 //     console.log(item)
@@ -8,8 +8,41 @@ let mySet = new Set(['Harry','Ron','hermione','Hagrid','Harry']);
 //     console.log(item)
 // })
 
-let filtered = [...mySet].filter((item)=>{
-    return item.startsWith('H')
+// let filtered = [...mySet].filter((item)=>{
+//     return item.startsWith('H')
+// })
+
+// console.log(filtered)
+
+let superHeroe = new Map();
+
+superHeroe.set('Batman',{
+    realName:'Bruce Wayne',
+    power:'Millonaire',
+    weakness:'None'
 })
 
-console.log(filtered)
+superHeroe.set('Superman',{
+    realName:'Clark Kent',
+    power:'All',
+    weakness:'Not having power'
+})
+
+// for(let key of superHeroe){
+//     // console.log(key)
+//     // console.log(value)
+// }
+
+// superHeroe.forEach((value,key)=>{
+//     console.log(key)
+//     console.log(value)
+// })
+
+// console.log(Array.from(superHeroe.keys()));
+// console.log(Array.from(superHeroe.values()));
+
+let names = Array.from(superHeroe.values()).map(item =>{
+   return item.realName
+})
+
+console.log(names)
